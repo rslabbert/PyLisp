683469c95b7bab24c2323592c4b0a48be88c476e
Rubear <rubenslabbert@gmail.com>
2015-04-09 14:35:55 +1000

    Fixed lists

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # PyLisp
 
 ## TODO
-* Make function such as max and min operate on lists instead of args
+* Importing local files
 * Write more tests
 * Implement almost the entire scheme standard
 * Start work on a standard library
-
-## Functions to add
-* list: head/car, tail/cdr, init, last, length, null, reverse, take, index, drop, max, min, sum, product, elem, range, zip
-* TEST THESE
--- src/core/pylTypes.py
+++ src/core/pylTypes.py
@@ -6,12 +6,53 @@ import types
 # Testing for types is in here instead of inside conditionals to keep common elements connected
 # Lambdas are used instead of builtin functions since the argument count
 # can then be accessed which is used for currying
-pylTypes = {
+booleans = {
     "boolean?": lambda x: isinstance(x, bool),
-    "symbol?": lambda x: isinstance(x, tokens.symbol.Symbol),
-    "null?": lambda x: x is None,
-    "number?": lambda x: isinstance(x, (int, float)),
-    "procedure?": lambda x: isinstance(x, (tokens.function.Function, partial, types.BuiltinFunctionType, types.LambdaType)),
-    "string?": lambda x: isinstance(x, str),
-    "list?": lambda x: isinstance(x, tokens.lst.Lst)
 }
+
+symbols = {
+    "symbol?": lambda x: isinstance(x, tokens.symbol.Symbol),
+}
+
+numbers = {
+    "number?": lambda x: isinstance(x, (int, float)),
+}
+
+functions = {
+    "procedure?": lambda x: isinstance(x, (tokens.function.Function, partial, types.BuiltinFunctionType, types.LambdaType)),
+}
+
+strings = {
+    "string?": lambda x: isinstance(x, str),
+}
+
+generic = {
+    "null?": lambda x: x is None,
+}
+
+lists = {
+    # Car and cdr for compatibility with other schemes
+    "list?": lambda x: isinstance(x, tokens.lst.Lst),
+    "car": lambda x: x[0],
+    "cdr": lambda x: x[1:],
+    "head": lambda x: x[0],
+    "tail": lambda x: x[1:],
+    "init": lambda x: x[:-1],
+    "last": lambda x: x[-1],
+    "length": lambda x: len(x),
+    "append": lambda x, y: x.extend(y) if isinstance(y, tokens.lst.Lst) else x.append(y),
+    "reverse": lambda x: x.reverse(),
+    "take": lambda x, y: y[:x],
+    "index": lambda x, y: y.index(x),
+    "elem?": lambda x, y: not y.index(x) == -1,
+    "zip": lambda x, y: tokens.lst.Lst(*zip(x, y))
+}
+
+pylTypes = {}
+pylTypes.update(booleans)
+pylTypes.update(symbols)
+pylTypes.update(numbers)
+pylTypes.update(functions)
+pylTypes.update(strings)
+pylTypes.update(generic)
+pylTypes.update(lists)
